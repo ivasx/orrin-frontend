bed7645a7d2e7e46c82bc275d527ef9262d4e269
ivasx <ambroziak.v.ivan@gmail.com>
2025-10-09 23:25:44 +0300

    Adjust footer position when player is visible

--- orrin-frontend/src/layouts/MainLayout.jsx
+++ orrin-frontend/src/layouts/MainLayout.jsx
@@ -1,22 +1,28 @@
 import { useState } from 'react';
-import { Outlet } from 'react-router-dom'; // <--- Імпортуємо Outlet
+import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header.jsx';
 import Sidebar from '../components/Sidebar/Sidebar.jsx';
 import BottomPlayer from '../components/AudioPlayerContext/BottomPlayer.jsx';
+import { useAudioPlayer } from '../components/AudioPlayerContext/AudioPlayerContext.jsx';
 
 export default function MainLayout() {
   const [sidebarOpen, setSidebarOpen] = useState(true);
+  const { currentTrack } = useAudioPlayer();
 
-  return (
-    <div className="AppContainer">
-      <Header onMenuToggle={() => setSidebarOpen(!sidebarOpen)} />
-      <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
-      <div className={`main-content ${sidebarOpen ? 'main-content--shifted' : 'main-content--full'}`}>
-        <main className="main-wrapper">
-          <Outlet />
-        </main>
-      </div>
-      <BottomPlayer />
-    </div>
-  );
+    return (
+        <div className="AppContainer">
+            <Header onMenuToggle={() => setSidebarOpen(!sidebarOpen)} />
+            <Sidebar
+                isOpen={sidebarOpen}
+                onClose={() => setSidebarOpen(false)}
+                isPlayerVisible={!!currentTrack}
+            />
+            <div className={`main-content ${sidebarOpen ? 'main-content--shifted' : 'main-content--full'}`}>
+                <main className="main-wrapper">
+                    <Outlet />
+                </main>
+            </div>
+            <BottomPlayer />
+        </div>
+    );
 }
