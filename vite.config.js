1c94e337ecba091ba16dccb48b78dac189848756
ivasx <ambroziak.v.ivan@gmail.com>
2025-10-21 22:28:37 +0300

    Add vite-plugin-sitemap for sitemap generation"

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { sitemap } from 'vite-plugin-sitemap'
+import { ways } from './src/data';
+const dynamicRoutes = ways.map(track => `/track/${track.trackId}`);
 
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        sitemap({
+            hostname: 'https://orrin-yl1x.onrender.com',
+            exclude: ['/login', '/register'],
+            dynamicRoutes: dynamicRoutes,
+
+        })
+    ],
     server: {
         watch: {
             usePolling: true,
